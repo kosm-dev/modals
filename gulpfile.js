26398b82dc0cb6e535c17660801b6bfd6c2531ea
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2025-03-31 16:39:46 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const browsersync = require("browser-sync");
+const sass = require("gulp-sass")(require('sass'));
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+
+const dist = "./dist/";
+
+gulp.task("copy-html", () => {
+  return gulp.src("./src/index.html")
+    .pipe(gulp.dest(dist))
+    .pipe(browsersync.stream());
+});
+
+gulp.task("build-js", () => {
+  return gulp.src("./src/js/main.js")
+    .pipe(webpack({
+      mode: 'development',
+      output: {
+        filename: 'script.js'
+      },
+      watch: false,
+      devtool: "source-map",
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [['@babel/preset-env', {
+                  debug: true,
+                  corejs: 3,
+                  useBuiltIns: "usage"
+                }]]
+              }
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest(dist))
+    .on("end", browsersync.reload);
+});
+
+gulp.task("copy-assets", () => {
+  return gulp
+    .src("./src/assets/**/*.*", { encoding: false })
+    .pipe(gulp.dest(dist + "/assets"))
+    .on("end", browsersync.reload);
+});
+
+// Задача для компиляции и минификации SCSS
+gulp.task("build-scss", () => {
+  return gulp.src("./src/scss/**/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({ compatibility: 'ie8' })) // Минификация
+    .pipe(gulp.dest(dist)) // Помещаем CSS прямо в корень dist
+    .pipe(browsersync.stream());
+});
+
+// Добавляем наблюдение за файлами SCSS
+gulp.task("watch", () => {
+  browsersync.init({
+    server: "./dist/",
+    port: 4000,
+    notify: true
+  });
+
+  gulp.watch("./src/index.html", gulp.parallel("copy-html"));
+  gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
+  gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+  gulp.watch("./src/scss/**/*.scss", gulp.parallel("build-scss"));
+});
+
+// Объединенная сборка
+gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js", "build-scss"));
+
+// Production JavaScript
+gulp.task("build-prod-js", () => {
+  return gulp.src("./src/js/main.js")
+    .pipe(webpack({
+      mode: 'production',
+      output: {
+        filename: 'script.js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [['@babel/preset-env', {
+                  corejs: 3,
+                  useBuiltIns: "usage"
+                }]]
+              }
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task("default", gulp.parallel("watch", "build"));
